9d3fe78d8c89809e63221ec943e0c5564fe291c9
me-abumuhammad <com.koloboot@gmail.com>
2021-04-17 03:58:37 +0700

    delete transaction fix

---
+++ controller/muhsinincontroller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Abu Muhammad on 4/8/21.
+//
+
+#include "muhsinincontroller.h"
---
+++ controller/muhsinincontroller.h
@@ -0,0 +1,14 @@
+//
+// Created by Abu Muhammad on 4/8/21.
+//
+
+#ifndef WEBQA_MUHSININCONTROLLER_H
+#define WEBQA_MUHSININCONTROLLER_H
+
+
+class muhsinincontroller {
+
+};
+
+
+#endif //WEBQA_MUHSININCONTROLLER_H
